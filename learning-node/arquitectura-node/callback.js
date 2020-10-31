c8353dee43e93018202abe87f957ce9613a2fa33
Cristopher Paiva <josuepaiva7@gmail.com>
2020-10-31 09:15:12 -0300

    start workspace
    callback

---
+++ learning-node/arquitectura-node/callback.js
@@ -0,0 +1,18 @@
+const asyncCallback = function (cb) {
+    setInterval(() => {
+        if (Math.random() < 0.5) {
+            return cb(null, 'Hello World');
+        } else {
+            cb(new Error('Hello Error'));
+        }
+    }, 2000)
+};
+
+asyncCallback((err, msg) => {
+    if (err) {
+        console.log('error', err);
+    } else {
+        console.log('message', msg);
+    }
+});
+
